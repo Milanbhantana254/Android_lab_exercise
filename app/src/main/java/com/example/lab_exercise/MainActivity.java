049964396cc448ede48d6e833864ca28bf926d3c
aaditya <roshangautam213@gmail.com>
2021-11-28 10:29:25 +0545

    Hello Toast

--- app/src/main/java/com/example/lab_exercise/MainActivity.java
+++ app/src/main/java/com/example/lab_exercise/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.lab_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Hello_Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
